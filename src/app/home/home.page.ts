9663b97771675bd4f872d29469d2736e9dbb5046
Robert Thompson <robertpt@hotmail.co.uk>
2019-05-14 20:41:25 +0100

    POC completed for getting data from server
    can obtain JSON from my web server long with images and display them in cards

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+	
+  postData: any;
 
+  constructor(private http: HttpClient) { 
+    this.http.get('https://app.roberthompson.co.uk/meme-app/example.json').subscribe((response) => {
+      console.log(response[0].memeID);
+      this.postData = response;
+      console.log(this.postData);
+    });
+
+    /*
+    this.postData = this.http.get('https://app.roberthompson.co.uk/meme-app/example.json');
+    this.postData.subscribe(data => {
+      console.log(data);
+    })
+    //console.log(this.postData);
+    */
+  }
 }

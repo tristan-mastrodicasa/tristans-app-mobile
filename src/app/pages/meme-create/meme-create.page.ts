d6e76539247634c53c8ad88882f7b64413ed9a2d
Tristan <ghoststeam217@gmail.com>
2019-08-26 22:19:20 +1200

    FIXED: Added back id parameter for meme-create

--- src/app/pages/meme-create/meme-create.page.ts
+++ src/app/pages/meme-create/meme-create.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// import { ActivatedRoute } from '@angular/router';
 
 import { DynamicScriptLoaderService } from '../../services/dynamic-script-loader/dynamic-script-loader.service';
 
@@ -14,13 +15,16 @@ export class MemeCreatePage implements OnInit {
   public image: string;
 
   constructor(
-    private scriptLoader: DynamicScriptLoaderService
+    private scriptLoader: DynamicScriptLoaderService,
+    // private route: ActivatedRoute
   ) { }
 
   /**
    * Collect the canvas image to modify
    */
   public ngOnInit() {
+
+    // const id = this.route.snapshot.paramMap.get('id');
 
     /** @todo Here is where we would construct the URL to call the API and return the image for the canvas id */
     this.image = 'https://assets.entrepreneur.com/content/3x2/2000/20180703190744-rollsafe-meme.jpeg?width=700&crop=2:1';

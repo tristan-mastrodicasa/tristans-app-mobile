8a5f612f9b1c022a880738376c12f576f660f369
Tristan <ghoststeam217@gmail.com>
2019-09-19 14:58:31 +1200

    CHANGED: Call image from server to display in the meme create view

--- src/app/pages/meme-create/meme-create.page.ts
+++ src/app/pages/meme-create/meme-create.page.ts
@@ -24,20 +24,9 @@ export class MemeCreatePage {
 
     const id = parseInt(this.route.snapshot.paramMap.get('id'), 10);
 
-    /** @todo Here is where we would construct the URL to call the API and return the image for the canvas id */
-
     this.http.getCanvasById(id).toPromise().then((res) => {
 
-      console.log(this.image);
-      // this.image = res.imagePath;
-      const images = [
-        'http://192.168.1.16:3000/api/canvas/image/eba73eb72985cf651aff8a80bc4761f5',
-        'http://192.168.1.16:3000/api/canvas/image/d6856edf4de9ee13f849c6f983f9646f',
-      ];
-      this.image = images[Math.floor(Math.random() * images.length)];
-      console.log('loaded');
-
-      console.log(this.image);
+      this.image = res.imagePath;
 
     });
 

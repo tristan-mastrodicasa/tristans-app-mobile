2c7684e60abac0f57a8e8d20827f8d2404b98eba
Tristan <ghoststeam217@gmail.com>
2019-09-21 20:38:08 +1200

    FIXED: Untracked files

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-import { Env } from './env.interface';
-
-export const environment: Env = {
-  production: true,
-  serveFromCache: false,
-  serverUrl: 'http://locahost:8080/api/',
-  google_client_id: '839993677318-0nc1mhc3vvqhtq4ienhqq6ntej0k4rr8.apps.googleusercontent.com',
-};

5eda3161aae9b2b67c0e3cfb6140f6c8ba1fa38e
Tristan <ghoststeam217@gmail.com>
2019-10-23 12:01:38 +1300

    ADDED: Environment file, will change later

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+import { IEnv } from './env.interface';
+
+/**
+ * @todo When the team expands find a way to distribute only the production
+ * configuration and interface for dev envrionment
+ */
+export const environment: IEnv = {
+  production: false,
+  primaryWebsite: 'http://locahost:8080/',
+  serveFromCache: false,
+  includeCache: true,
+  serverUrl: 'http://192.168.1.11:3000/api/',
+  google_client_id: '839993677318-0nc1mhc3vvqhtq4ienhqq6ntej0k4rr8.apps.googleusercontent.com',
+};

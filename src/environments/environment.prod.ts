dc9f47e7344167fe04f2ec0ed23ca660a1d91d77
Tristan <ghoststeam217@gmail.com>
2019-10-23 18:40:12 +1300

    CHANGED: Production variables changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,9 @@ import { IEnv } from './env.interface';
 
 export const environment: IEnv = {
   production: true,
-  primaryWebsite: 'http://locahost:8080/',
+  primaryWebsite: 'https://tristansapp.com/',
   serveFromCache: false,
   includeCache: false,
-  serverUrl: 'http://tristansappserver-env.rypnfd8tvh.ap-southeast-2.elasticbeanstalk.com/api/',
+  serverUrl: 'https://tristansapp.com/api/',
   google_client_id: '839993677318-0nc1mhc3vvqhtq4ienhqq6ntej0k4rr8.apps.googleusercontent.com',
 };
